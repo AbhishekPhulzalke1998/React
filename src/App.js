656ef90d80a8220bcf4fbe9955a7d3dc23cbb971
zAbhishekPhulzalke1998"git config --global user.name git config --global user.email "abhishek.phulzalke@prorigosoftware.comgit config --global user.name zAbhishekPhulzalke1998git config --global user.name git config --global user.email abhishek.phulzalke@prorigosoftware.comgit config --global user.name z'AbhishekPhulzalke1998 <abhishek.phulzalke@prorigosoftware.comgit config --global user.name z'AbhishekPhulzalke1998>
2023-12-14 07:40:59 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+import demo from './Componets/demo';
+
+function App() {
+  return (
+    <div className="App">
+         <demo></demo>
+        <demo name ="john"/>
+        <demo></demo>
+        <demo name ="smith"/>
+        <demo></demo>
+
+       
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componets/man.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Man(){
+   return <h1>There is one man</h1>
+
+}
+ export default Man;
